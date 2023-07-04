a91078896784b0dd5490f30b730231e5d77f5f30
thayrov <thayrovg@gmail.com>
2023-07-04 10:22:17 -0600

    add dependencies, .prettierrc config and chakra UI theme config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './index.css';
+
+import App from './App.jsx';
+import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { extendTheme } from '@chakra-ui/react';
+
+const colors = {
+    brand: {
+        white: '#FEFFF6',
+        yellow: '#F4F1D4',
+        black: '#414141',
+        'light-green': '#E0E7C0',
+        'dark-green': '#7B8B28',
+    },
+};
+const theme = extendTheme({ colors });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <ChakraProvider theme={theme}>
+            <App />
+        </ChakraProvider>
+    </React.StrictMode>
+);
