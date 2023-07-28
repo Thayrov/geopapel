41274aa2d70ad3707d36e81eb427b3cd8275ed72
thayrov <thayrovg@gmail.com>
2023-07-28 15:50:29 -0600

    delete notion.js

--- client/notion.js
+++
@@ -1,6 +0,0 @@
-export const getBanners = async ({ filterBy } = {}) => {
-    const response = await fetch(`/api/banners?filterBy=${filterBy}`);
-    const banners = await response.json();
-
-    return banners;
-};
