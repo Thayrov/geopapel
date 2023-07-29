9556a44266a6f9ec3df67b9908e5abfde78e1cb5
thayrov <thayrovg@gmail.com>
2023-07-29 02:13:26 -0600

    code testimonials related components logic

--- client/src/components/Testimonials/Testimonials.jsx
+++ client/src/components/Testimonials/Testimonials.jsx
@@ -1,8 +1,32 @@
+import { Box, SimpleGrid, Text } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+
+import TestimonialItem from './TestimonialItem';
+
 const Testimonials = () => {
+    const [testimonials, setTestimonials] = useState([]);
+
+    useEffect(() => {
+        const url = `${import.meta.env.VITE_API_URL}/testimonials`;
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => setTestimonials(data))
+            .catch((err) => console.log(err));
+    }, []);
     return (
-        <section>
-            <h1>Testimonials</h1>
-        </section>
+        <Box p={{ base: '10', lg: '100' }}>
+            <Text fontSize={{ base: '28px', md: '32px', lg: '48px' }} fontWeight="400">
+                Testimonios
+            </Text>
+            <Text fontSize={{ base: '42px', md: '64px', lg: '72px' }} fontWeight="600">
+                Geo Papel
+            </Text>
+            <SimpleGrid columns={{ base: 1, md: 1, lg: 3 }} spacing="48px">
+                {testimonials.map((testimonial, index) => (
+                    <TestimonialItem key={index} testimonial={testimonial} />
+                ))}
+            </SimpleGrid>
+        </Box>
     );
 };
 
